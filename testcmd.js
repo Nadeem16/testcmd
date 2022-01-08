19f27133f1707ea0397c9173329248d408b135e8
Nadeem Khan <nadeemkhan1696@gmail.com>
2022-01-08 16:59:46 +0300

    commit

--- testcmd.js
+++ testcmd.js
@@ -1,4 +1,4 @@
 console.log("hello");
 // i m making some changes to it 
 
-// i have created  a anew branch 
+// i have created  a anew branch gitgit
--- testone.js
+++ testone.js
@@ -4,3 +4,8 @@ console.log("this is new file");
 // i m creating a new branch 
 
  console.log("the is my 2nd branch");
+
+ // this is the third branch 
+
+ console.log("3rd branch");
+ 
