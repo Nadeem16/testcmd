62d9f42e79aaeb224cc51ce7bb70b51a4b9a623f
Nadeem Khan <nadeemkhan1696@gmail.com>
2022-01-14 16:28:44 +0300

    Task

---
+++ Assign1.js
@@ -0,0 +1,8 @@
+var Name = " Nadeem Khan";
+var Age = 26 ;
+var MartialStatus = "single";
+
+console.log(Name);
+console.log(Age);
+console.log(MartialStatus);
+
---
+++ Assign2.js
@@ -0,0 +1,7 @@
+var gender = "Male";
+var height = 6.1;
+var nonveg = "Chicken";
+
+console.log(gender);
+console.log(height);
+console.log(nonveg);
